ea2b758d132beacd766b3278dd5e6cdef7c68f61
José B <jose.bece.r2@gmail.com>
2025-12-14 14:46:04 -0600

    Mejora para empresa para organizacion de codigo

--- Día_1/Bases_cpp.cpp
+++ Día_1/Bases_cpp.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main(){
     string nombre;
     int edad;
+    
     cout << "Nombre: \n";
     getline(cin, nombre);
+
     cout << "Edad: \n";
     cin >> edad;
-    cout << "Hola " << nombre <<" en 20 anios tendras " << edad + 20 << " anios";
+
+    cout << "Hola " << nombre 
+         <<" en 20 anios tendras " 
+         << edad + 20 << " anios\n";
 }
