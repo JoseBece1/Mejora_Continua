5abbe801ae8e767b350308cb94fbe538d2d3758b
José B <jose.bece.r2@gmail.com>
2025-12-14 14:43:45 -0600

    Agregar el cpp, no lo habia agregado por no saber >;(

---
+++ Día_1/Bases_cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string nombre;
+    int edad;
+    cout << "Nombre: \n";
+    getline(cin, nombre);
+    cout << "Edad: \n";
+    cin >> edad;
+    cout << "Hola " << nombre <<" en 20 anios tendras " << edad + 20 << " anios";
+}
