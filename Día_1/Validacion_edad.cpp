be1272173f9ea9d3a357bd897ff4730bd5a78283
José B <jose.bece.r2@gmail.com>
2025-12-14 17:38:45 -0600

    Validacion de entrada con std::cin.fail()

--- Día_1/Validacion_edad.cpp
+++ Día_1/Validacion_edad.cpp
@@ -6,7 +6,11 @@ int main(){
     std::cout << "Dime tu edad:\n";
     std::cin >> edad;
 
+    if(std::cin.fail()) std::cout << "Entrada invalida";
+
     if(edad < 0) std::cout << "Edad invalida\n";
     else if (edad < 18) std::cout << "Menor de edad\n";
     else std::cout << "Mayor de edad\n";    
+
+    return 0;
 }
